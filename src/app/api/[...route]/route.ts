c478567cb3ec79422c3ca461a51886dd4a84d6e3
Mike Odnis <airwolf635@gmail.com>
2024-11-27 17:41:32 -0500

    Quick commit

--- src/app/api/[...route]/route.ts
+++ src/app/api/[...route]/route.ts
@@ -2,4 +2,11 @@ import { honoRouter } from '@/server';
 import { handle } from 'hono/vercel';
 
 export const dynamic = 'force-dynamic';
-export const GET = handle(honoRouter);
+
+const handler = handle(honoRouter);
+
+export const GET = handler;
+export const POST = handler;
+export const PUT = handler;
+export const DELETE = handler;
+export const PATCH = handler;
